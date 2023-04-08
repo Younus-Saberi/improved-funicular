6894b4387790f1683a7b1741197851d4abc11f0f
Younus-Saberi <younussaberi@gmail.com>
2023-04-08 19:33:48 +0530

    Revert "add ec2 vars"
    This reverts commit b970dee2bcd976c3fe1658d785ba47b9fec1bc85.

--- modules/ec2/var.tf
+++ modules/ec2/var.tf
@@ -11,33 +11,3 @@ variable "instance_type" {
 }
 
 variable "subnet_id" {}
-
-variable "region" {
-  description = "The AWS region where resources will be created"
-}
-
-variable "vpc_cidr_block" {
-  description = "The CIDR block for the VPC"
-}
-
-variable "public_subnet_cidr_block" {
-  description = "The CIDR block for the public subnet"
-}
-
-variable "private_subnet_cidr_block" {
-  description = "The CIDR block for the private subnet"
-}
-
-variable "web_server_instance_type" {
-  description = "The EC2 instance type for the web server"
-  default = "t2.micro"
-}
-
-variable "web_server_ami" {
-  description = "The ID of the AMI to use for the web server"
-  default = "ami-007855ac798b5175e"
-}
-
-variable "web_server_key_name" {
-  description = "The name of the key pair to use for SSH access to the web server"
-}
