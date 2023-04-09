0600a0555afc466e75f6e3c21718f8b08a4a46ee
Younus-Saberi <younussaberi@gmail.com>
2023-04-09 10:29:50 +0530

    update root main.tf

--- dev/main.tf
+++ dev/main.tf
@@ -19,3 +19,5 @@ module "my_ec2" {
   instance_type = "t2.micro"
   subnet_id     = "${module.my_vpc.subnet_id}"
 }
+
+ 
--- main.tf
+++ main.tf
@@ -137,15 +137,11 @@ output "server_public_ip" {
 resource "aws_instance" "web-server-instance" {
   ami               = "ami-007855ac798b5175e"
   instance_type     = "t2.micro"
-  availability_zone = "us-east-1a"
   key_name          = "my-custom-server"
-  #subnet_id = aws_subnet.private_subnet.id
-  # vpc_security_group_ids = ["${aws_security_group.ssh-allowed.id}"]
+  subnet_id         = aws_subnet.private_subnet.id
+  vpc_security_group_ids = ["${aws_security_group.allow_web.id}"]
 
-  network_interface {
-    device_index         = 0
-    network_interface_id = aws_network_interface.web-server-nic.id
-  }
+  
 
   user_data = <<-EOF
                 #!/bin/bash
